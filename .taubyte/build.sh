448d70e192c4fc0c39e2e43d6c38d3be199ea659
ZaouiAmine <76101501+ZaouiAmine@users.noreply.github.com>
2026-03-11 15:56:01 +0100

    Use generated API URL

--- .taubyte/build.sh
+++ .taubyte/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm install
 npm audit fix --omit=dev 2>/dev/null || true
-VITE_API_URL=https://imposter.scaliny.com npm run build
+VITE_API_URL=https://hf1af5qa0.cloud.scaliny.com npm run build
 cp -r dist/* /out
